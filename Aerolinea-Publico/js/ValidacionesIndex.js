b551b98239fcc38e514e9f74816f44b7b40e9a4a
Jose Manuel <josemhp709@gmail.com>
2017-04-20 19:16:01 -0600

    modificando

--- Aerolinea-Publico/js/ValidacionesIndex.js
+++ Aerolinea-Publico/js/ValidacionesIndex.js
@@ -1,9 +1,21 @@
 var arrayUsuarios;
 var user;
 function pageLoad(event) {
-    document.getElementById("formulario").reset();
+    document.getElementById("formularioSesion").reset();
     addEventListeners();
     loadList();
+}
+function addEventListeners() {
+    let correo= document.getElementById("correo");
+    let password = document.getElementById("password");
+   
+	password.addEventListener("focus",doFocus);
+	password.addEventListener("blur",doBlur); 
+	correo.addEventListener("focus",doFocus);
+	correo.addEventListener("blur",doBlur); 
+    
+    let formularioSesion = document.getElementById("formularioSesion");
+    formularioSesion.addEventListener("submit", iniciarSesion);
 }
 /*-----------------------PARTE DE INICIO DE SESION----------------------------------- */
 function loadList() {
@@ -12,6 +24,18 @@ function loadList() {
         arrayUsuarios = [];
         Storage.store("Usuarios", arrayUsuarios);
     }
+}
+function iniciarSesion(){
+    let correo= document.getElementById("correo");
+    let password = document.getElementById("password");
+    if(usuarioCorrecto(correo,password)){
+        window.alert("Bienvenido");
+    }
+}
+
+function usuarioCorrecto(correo, password) {
+    let index = arrayUsuarios.findIndex( ((est) => est.correo === correo.value) && ((est) => est.contrasena === password.value) );
+    return (index != -1)? false : true;
 }
 /**----------------------------------------------------------------------------------------------- */
 document.addEventListener("DOMContentLoaded", pageLoad)
