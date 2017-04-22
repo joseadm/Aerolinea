b26477b46372f6191f10700b5771f2253cba85ac
Chenx2a <chen.chen96@hotmail.com>
2017-04-21 23:09:17 -0600

    pago.js
    validacion

--- Aerolinea-Publico/js/ValidacionPago.js
+++ Aerolinea-Publico/js/ValidacionPago.js
@@ -1,16 +1,29 @@
 function pageLoad(event) {
+  var btnAceptar = document.getElementById("aceptar");
+  var btnCancelar = document.getElementById("cancelar");
+  btnAceptar.addEventListener("click",pagar);
 
 }
 
-function validar(campo){
-  if ((document.forms[0][campo].value.length==0) ||
-  (document.forms[0][campo].value==null)) {
+function validar(){
+  var x = document.forms["formPago"]["numTarjeta"].value;
+  if (x == "") {
+      alert("Por favor ingrese el numero de tarjeta");
+      return false;
+  }
   return true;
 }
-else { return false; }
+
+function pagar(){
+  var card = document.getElementById("numTarjeta");
+  if(validar(card))
+    alert("Pago realizado.");
 }
 
-function 
+function cancelar(){
+
+}
+
 
 /**----------------------------------------------------------------------------------------------- */
 document.addEventListener("DOMContentLoaded", pageLoad)
