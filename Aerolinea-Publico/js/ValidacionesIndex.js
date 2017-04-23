12590811a2ab38e23107ee4d9a9534e0e60210f8
Jose Manuel <josemhp709@gmail.com>
2017-04-22 20:57:32 -0600

    valdiaciones Arregladas

--- Aerolinea-Publico/js/ValidacionesIndex.js
+++ Aerolinea-Publico/js/ValidacionesIndex.js
@@ -7,7 +7,7 @@ var arrayAeropuertos;
 let user;
 function pageLoad(event) {
     loadCarousel();
-    crearBotones();
+    cargarBotones();
     addEventListeners();
     loadListUsuarios();
     loadListCiudades();
@@ -20,7 +20,7 @@ function loadCarousel() {
 }
 
 function addEventListeners() {
-    let correo= document.getElementById("correo");
+     let correo= document.getElementById("correo");
     let password = document.getElementById("password");
     let aero_origen = document.getElementById("aero_origen");
     let aero_destino = document.getElementById("aero_destino");
@@ -36,8 +36,6 @@ function addEventListeners() {
 	correo.addEventListener("focus",doFocus);
 	correo.addEventListener("blur",doBlur);
 
-    /*let formularioSesion = document.getElementById("formularioSesion");
-    formularioSesion.addEventListener("submit", iniciarSesion);*/
     var incioSesionBoton= document.getElementById("inicioSesion");
     incioSesionBoton.addEventListener("click",iniciarSesion);
     var cerrarSesion= document.getElementById("cerrarSesion");
@@ -95,62 +93,15 @@ function aeropuertos(){
 
 }
 /*-----------------------PARTE DE INICIO DE SESION----------------------------------- */
-function crearBotones(){
-        let form = document.getElementById("formularioSesion");
-
-        let input = document.createElement("input");
-        input.setAttribute("id","correo");
-        input.setAttribute("name","correo");
-        input.setAttribute("placeholder","Email");
-        input.setAttribute("class","form-control");
-        input.setAttribute("type","text");
-        form.appendChild(input);
-
-        let input2 = document.createElement("input");
-        input2.setAttribute("id","password");
-        input2.setAttribute("name","password");
-        input2.setAttribute("placeholder","Contraseña");
-        input2.setAttribute("class","form-control");
-        input2.setAttribute("type","password");
-        form.appendChild(input2);
-
-        let button = document.createElement("button");
-        button.setAttribute("id","inicioSesion");
-        button.setAttribute("type","button");
-        button.setAttribute("class","btn btn-success");
-        let botonInicio= "Iniciar Sesion";
-        button.innerHTML = botonInicio;
-        form.appendChild(button);
-
-        let a = document.createElement("a");
-        a.setAttribute("href","registro.html");
-        a.setAttribute("id","crearCuenta");
-        a.setAttribute("type","button");
-        a.setAttribute("class","btn btn-primary");
-        let botonCrear= "Crear cuenta"
-        a.innerHTML = botonCrear;
-        form.appendChild(a);
-
-        let button2 = document.createElement("button");
-        button2.setAttribute("id","cerrarSesion");
-        button2.setAttribute("type","button");
-        button2.setAttribute("class","btn btn-danger");
-        let botonInicio2= "Cerrar Sesion";
-        button2.innerHTML = botonInicio2;
-        form.appendChild(button2);
-
-       cargarBotones();
-
-}
 function cargarBotones(){
     if(user!=1){ /*Nadie a iniciado sesion */
-        document.getElementById("cerrarSesion").style.display = 'none';
+        document.getElementById("ulPrincipal").style.display = 'none';
     }else{
         document.getElementById("correo").style.display = 'none';
         document.getElementById("password").style.display = 'none';
         document.getElementById("crearCuenta").style.display = 'none';
         document.getElementById("inicioSesion").style.display = 'none';
-        document.getElementById("cerrarSesion").style.display = 'initial';
+        document.getElementById("ulPrincipal").style.display = 'initial';
     }
 }
 function loadListUsuarios() {
