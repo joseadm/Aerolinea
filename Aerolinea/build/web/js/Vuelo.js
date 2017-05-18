ff9d130795a4e34c99b16eafd98e0d0554410606
AndreyCh <andreychaverri5757@gmail.com>
2017-05-17 22:31:26 -0600

    cambios en el archivo Vuelo y Viaje js

--- Aerolinea/build/web/js/Vuelo.js
+++ Aerolinea/build/web/js/Vuelo.js
@@ -1,8 +1,16 @@
-function Vuelo(codigo,ciudad_origen,ciudad_destino,avion,fecha,estado, precio){
-	this.Vuelo(codigo,ciudad_origen,ciudad_destino,avion,fecha,estado, precio);
+function Vuelo(codigo,ciudad_origen,ciudad_destino,avion,fecha,estado, precio,duracion){
+	this.Vuelo(codigo,ciudad_origen,ciudad_destino,avion,fecha,estado, precio,duracion);
 }
 Vuelo.prototype={
-	Vuelo:function(codigo,ciudad_origen,ciudad_destino,avion,fecha,estado,precio){
+        codigo:"",
+        ciudad_origen:"",
+        ciudad_destino:"",
+        avion:"",
+        fecha:"",
+        estado:"",
+        precio:"",
+        duracion:"",
+	Vuelo:function(codigo,ciudad_origen,ciudad_destino,avion,fecha,estado,precio,duracion){
 		this.codigo=codigo;
 		this.ciudad_origen=ciudad_origen;
 		this.ciudad_destino=ciudad_destino;
@@ -10,5 +18,28 @@ Vuelo.prototype={
 		this.fecha=fecha;
 		this.estado=estado;
 		this.precio=precio;
+                this.duracion = duracion;
+	},
+        toString:function(){
+	  return this.codigo;
 	}
 }
+Vuelo.from= function(plain){
+    vuelo = new Vuelo(plain.codigo,plain.ciudad_origen,plain.ciudad_destino,plain.avion,
+                      plain.fecha, plain.estado,plain.precio,plain.duracion);
+    return vuelo;
+};
+
+Vuelo.to = function(vuelo){
+    return{
+        _class : 'Vuelo',
+        codigo: vuelo.codigo,
+        ciudad_origen: vuelo.ciudad_origen,
+        ciudad_destino: vuelo.ciudad_destino,
+        avion: vuelo.avion,
+        fecha: vuelo.fecha,
+        estado: vuelo.estado,
+        precio: vuelo.precio,
+        duracion: vuelo.duracion
+    };
+};
--- Aerolinea/web/js/Viaje.js
+++ Aerolinea/web/js/Viaje.js
@@ -7,12 +7,31 @@ function Viaje(numero,avion,vuelo){
 	this.Viaje(numero,avion,vuelo);
 }
 Viaje.prototype={
+        numero: 0,
+        avion:"",
+        vuelo: 0,
 	Viaje:function(numero,avion,vuelo){
 		this.numero=numero;
 		this.avion=avion;
 		this.vuelo=vuelo;
+	},
+        toString:function(){
+	  return this.numero;
 	}
 }
+Viaje.from=function(plain){
+    viaje = new Viaje(plain.numero,plain.avion,plain.vuelo);
+    return viaje;
+};
+
+Viaje.to= function(viaje){
+    return{
+         _class : 'Viaje',
+         numero: viaje.numero,
+         avion: viaje.avion,
+         vuelo: viaje.vuelo
+    };
+};
 
 
 
