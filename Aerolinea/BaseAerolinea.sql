7fcf6d3ef9dc81c89095d2fccac274df8154b608
Jose Manuel <josemhp709@gmail.com>
2017-05-17 19:32:29 -0600

    SQL actualizado

--- Aerolinea/BaseAerolinea.sql
+++ Aerolinea/BaseAerolinea.sql
@@ -32,24 +32,6 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Aerolinea`.`Pasajero`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Aerolinea`.`Pasajero` (
-  `cedula` INT NOT NULL,
-  `nombre` VARCHAR(45) NOT NULL,
-  `nombre_usuario` VARCHAR(45) NULL,
-  `pasaporte` INT NULL,
-  PRIMARY KEY (`cedula`),
-  INDEX `nombre_usuario_idx` (`nombre_usuario` ASC),
-  CONSTRAINT `nombre_usuario`
-    FOREIGN KEY (`nombre_usuario`)
-    REFERENCES `Aerolinea`.`Usuario` (`nombreUsuario`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
 -- Table `Aerolinea`.`Ciudad`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `Aerolinea`.`Ciudad` (
@@ -90,9 +72,9 @@ CREATE TABLE IF NOT EXISTS `Aerolinea`.`Avion` (
   `modelo` VARCHAR(45) NULL,
   `marca` VARCHAR(45) NULL,
   `anno` INT NULL,
-  `cantidadPasajeros` INT NULL,
-  `numeroFilas` INT NULL,
+  `numeroAsiento` INT NULL,
   `asientosPorFilas` INT NULL,
+  `numeroFilas` INT NULL,
   PRIMARY KEY (`placa`))
 ENGINE = InnoDB;
 
@@ -139,6 +121,25 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
+-- Table `Aerolinea`.`Tiquete`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `Aerolinea`.`Tiquete` (
+  `codigo` INT NOT NULL,
+  `nombre_usuario` VARCHAR(45) NULL,
+  `cedula_pasajero` INT NULL,
+  `nombre_pasajero` VARCHAR(45) NULL,
+  `pasaporte` INT NULL,
+  PRIMARY KEY (`codigo`),
+  INDEX `nombre_usuario_idx` (`nombre_usuario` ASC),
+  CONSTRAINT `nombre_usuario`
+    FOREIGN KEY (`nombre_usuario`)
+    REFERENCES `Aerolinea`.`Usuario` (`nombreUsuario`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
 -- Table `Aerolinea`.`Reservacion`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `Aerolinea`.`Reservacion` (
@@ -147,13 +148,14 @@ CREATE TABLE IF NOT EXISTS `Aerolinea`.`Reservacion` (
   `fechaRegreso` DATE NULL,
   `numero_viaje1` INT NULL,
   `numero_viaje2` INT NULL,
-  `cedula_pasajero` INT NULL,
   `codigo_asiento` INT NULL,
+  `codigo_tiquete` INT NULL,
+  `numeroAsiento` INT NULL,
   PRIMARY KEY (`codigo`),
   INDEX `numero_viaje1_idx` (`numero_viaje1` ASC),
   INDEX `numero_viaje2_idx` (`numero_viaje2` ASC),
-  INDEX `cedula_pasajero_idx` (`cedula_pasajero` ASC),
   INDEX `codigo_asiento_idx` (`codigo_asiento` ASC),
+  INDEX `codigo_tiquete_idx` (`codigo_tiquete` ASC),
   CONSTRAINT `numero_viaje1`
     FOREIGN KEY (`numero_viaje1`)
     REFERENCES `Aerolinea`.`Viaje` (`numeroviaje`)
@@ -164,31 +166,14 @@ CREATE TABLE IF NOT EXISTS `Aerolinea`.`Reservacion` (
     REFERENCES `Aerolinea`.`Viaje` (`numeroviaje`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `cedula_pasajero`
-    FOREIGN KEY (`cedula_pasajero`)
-    REFERENCES `Aerolinea`.`Pasajero` (`cedula`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
   CONSTRAINT `codigo_asiento`
     FOREIGN KEY (`codigo_asiento`)
     REFERENCES `Aerolinea`.`Asiento` (`codigo`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `Aerolinea`.`Tiquete`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Aerolinea`.`Tiquete` (
-  `codigo` INT NOT NULL,
-  `numeroAsiento` INT NULL,
-  `codigo_reservacion` INT NULL,
-  PRIMARY KEY (`codigo`),
-  INDEX `codigo_reservacion_idx` (`codigo_reservacion` ASC),
-  CONSTRAINT `codigo_reservacion`
-    FOREIGN KEY (`codigo_reservacion`)
-    REFERENCES `Aerolinea`.`Reservacion` (`codigo`)
+    ON UPDATE NO ACTION,
+  CONSTRAINT `codigo_tiquete`
+    FOREIGN KEY (`codigo_tiquete`)
+    REFERENCES `Aerolinea`.`Tiquete` (`codigo`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
