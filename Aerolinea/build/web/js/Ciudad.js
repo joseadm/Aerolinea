12a8f44a829142b2614a151c8e40dde6a69a9991
AndreyCh <andreychaverri5757@gmail.com>
2017-05-17 18:24:39 -0600

    cambios en el archivo Ciudad.js

--- Aerolinea/build/web/js/Ciudad.js
+++ Aerolinea/build/web/js/Ciudad.js
@@ -2,6 +2,9 @@ function Ciudad(codigo,nombre,pais){
 	this.Ciudad(codigo,nombre,pais);
 }
 Ciudad.prototype={
+        codigo:"",
+        nombre:"",
+        pais:"",
 	Ciudad:function(codigo,nombre,pais){
 		this.codigo=codigo;
 		this.nombre=nombre;
@@ -11,3 +14,16 @@ Ciudad.prototype={
 		return this.nombre;
 	}       
 }
+
+Ciudad.from= function(plain){
+    ciudad = new Ciudad(plain.codigo,plain.nombre,plain.pais);
+    return ciudad;
+};
+
+Ciudad.to= function(ciudad){
+    return{
+        codigo: ciudad.codigo,
+        nombre: ciudad.nombre,
+        pais: ciudad.pais
+    };
+};
