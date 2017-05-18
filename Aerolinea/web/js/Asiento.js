4fbe78a2eae7235ca21a79c731ec15e8abf26236
AndreyCh <andreychaverri5757@gmail.com>
2017-05-17 18:00:28 -0600

    cambiandos  las entidades de js

--- Aerolinea/web/js/Asiento.js
+++ Aerolinea/web/js/Asiento.js
@@ -3,14 +3,35 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-function Asiento(numero,estado,viaje){
-	this.Asiento(numero,estado,viaje);
+function Asiento(codigo,numero,estado,numero_viaje){
+	this.Asiento(codigo,numero,estado,numero_viaje);
 }
 Asiento.prototype={
-	Asiento:function(numero,estado,viaje){
-		this.numero=numero;
+        codigo:0,
+        numero:0,
+        estado:false,
+        numero_viaje:0,
+	Asiento:function(codigo,numero,estado,numero_viaje){
+		this.codigo= codigo;
+                this.numero=numero;
 		this.estado=estado;
-		this.viaje=viaje;
+		this.numero_viaje=numero_viaje;
+	},
+        toString:function(){
+	  return this.numero;
 	}
 }
+Asiento.from= function(plain){
+    asiento = new Asiento(plain.codigo,plain.numero,plain.estado,plain.numero_viaje);
+    return asiento;
+};
+Asiento.to= function(asiento){
+    return {
+        _class : 'Asiento',
+        codigo: asiento.codigo,
+        numero: asiento.numero,
+        estado: asiento.estado,
+        numero_viaje : asiento.numero_viaje
+    };
+};
 
