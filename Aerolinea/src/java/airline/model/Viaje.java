3d8a4fa1c5a5a41d187ac2993157629d33e110bd
Jose Manuel <josemhp709@gmail.com>
2017-05-18 22:48:39 -0600

    Fecha agregada al viaje

--- Aerolinea/src/java/airline/model/Viaje.java
+++ Aerolinea/src/java/airline/model/Viaje.java
@@ -5,17 +5,20 @@
  */
 package airline.model;
 import java.io.Serializable;
+import java.util.Date;
 /**
  *
  * @author AndreyCh
  */
 public class Viaje implements Serializable, Jsonable{
     int numero_viaje;
+    Date fecha;
     Avion avion;
     Vuelo vuelo;
 
-    public Viaje(int numero_viaje, Avion avion, Vuelo vuelo) {
+    public Viaje(int numero_viaje,Date fecha, Avion avion, Vuelo vuelo) {
         this.numero_viaje = numero_viaje;
+        this.fecha = fecha;
         this.avion = avion;
         this.vuelo = vuelo;
     }
@@ -25,6 +28,9 @@ public class Viaje implements Serializable, Jsonable{
 
     public int getNumero_viaje() {
         return numero_viaje;
+    }
+     public Date getFecha() {
+        return fecha;
     }
 
     public Avion getAvion() {
@@ -37,6 +43,9 @@ public class Viaje implements Serializable, Jsonable{
 
     public void setNumero_viaje(int numero_viaje) {
         this.numero_viaje = numero_viaje;
+    }
+    public void SetFecha(Date fecha) {
+        this.fecha = fecha;
     }
 
     public void setAvion(Avion avion) {
