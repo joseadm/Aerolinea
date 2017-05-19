805d8ba03e815bd5e464807fb9f5fe2ed4fc0573
Jose Manuel <josemhp709@gmail.com>
2017-05-18 22:45:14 -0600

    Airline model corregido con los cambios a las clases.java

--- Aerolinea/src/java/airline/model/AirlineModel.java
+++ Aerolinea/src/java/airline/model/AirlineModel.java
@@ -25,12 +25,12 @@ public class AirlineModel {
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         Date d = sdf.parse("03/08/2017");
         Vuelo[] promo = {
-        new Vuelo(1,new Ciudad("SJO","San Jose","Costa Rica"),new Ciudad("MIA","Miami","USA"),new Avion("TI-CR","Boeing","737-800","2005",180,30,6),d,true, 100,""),
-        new Vuelo(2,new Ciudad("MIA","Miami","USA"),new Ciudad("CAN","Cancun","Mexico"),new Avion("NV506","Boeing","737-400","2001",168,28,6),d,true,200,""),
-        new Vuelo(3,new Ciudad("MAD","Madrid","Espana"),new Ciudad("ROM","Roma","Italia"),new Avion("NVCR0314","Airbus","380-800","2012",450,50,9),d,true,200,""),
-        new Vuelo(4,new Ciudad("SJO","San Jose","Costa Rica"),new Ciudad("MAD","Madrid","Espana"),new Avion("TI-CR5698","Airbus","330-300","2005",297,33,9),d,true,100,""),
-        new Vuelo(5,new Ciudad("MIA","Miami","USA"),new Ciudad("SJO","San Jose","Costa Rica"),new Avion("TI-BGV","Boeing","737-600","2001",132,22,6),d,true,40,""),
-        new Vuelo(6,new Ciudad("VIE","Viena","Austria"),new Ciudad("MAD","Madrid","Espana"),new Avion("NV123","Boeing","747-400","2010",315,35,9),d,true,60,"")
+        new Vuelo(1,new Ciudad("SJO","San Jose","Costa Rica"),new Ciudad("MIA","Miami","USA"),true, 100,""),
+        new Vuelo(2,new Ciudad("MIA","Miami","USA"),new Ciudad("CAN","Cancun","Mexico"),true,200,""),
+        new Vuelo(3,new Ciudad("MAD","Madrid","Espana"),new Ciudad("ROM","Roma","Italia"),true,200,""),
+        new Vuelo(4,new Ciudad("SJO","San Jose","Costa Rica"),new Ciudad("MAD","Madrid","Espana"),true,100,""),
+        new Vuelo(5,new Ciudad("MIA","Miami","USA"),new Ciudad("SJO","San Jose","Costa Rica"),true,40,""),
+        new Vuelo(6,new Ciudad("VIE","Viena","Austria"),new Ciudad("MAD","Madrid","Espana"),true,60,"")
         };
         return new ArrayList(Arrays.asList(promo));
     }
@@ -60,24 +60,24 @@ public class AirlineModel {
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         Date d = sdf.parse("03/08/2017");
         Vuelo [] vuelos = {
-            new Vuelo(1,new Ciudad("SJO","San Jose","Costa Rica"),new Ciudad("MIA","Miami","USA"),new Avion("TI-CR","Boeing","737-800","2005",180,30,6),d,true, 100,""),
-            new Vuelo(2,new Ciudad("MIA","Miami","USA"),new Ciudad("CAN","Cancun","Mexico"),new Avion("NV506","Boeing","737-400","2001",168,28,6),d,true,200,""),
-            new Vuelo(3,new Ciudad("MAD","Madrid","Espana"),new Ciudad("ROM","Roma","Italia"),new Avion("NVCR0314","Airbus","380-800","2012",450,50,9),d,true,200,""),
-            new Vuelo(4,new Ciudad("SJO","San Jose","Costa Rica"),new Ciudad("MAD","Madrid","Espana"),new Avion("TI-CR5698","Airbus","330-300","2005",297,33,9),d,true,100,""),
-            new Vuelo(5,new Ciudad("MIA","Miami","USA"),new Ciudad("SJO","San Jose","Costa Rica"),new Avion("TI-BGV","Boeing","737-600","2001",132,22,6),d,true,40,""),
-            new Vuelo(6,new Ciudad("VIE","Viena","Austria"),new Ciudad("MAD","Madrid","Espana"),new Avion("NV123","Boeing","747-400","2010",315,35,9),d,true,60,""),
-            new Vuelo(7,new Ciudad("SJO","San Jose","Costa Rica"),new Ciudad("MIA","Miami","USA"),new Avion("TI-CR","Boeing","737-800","2005",180,30,6),d,true, 100,""),
-            new Vuelo(8,new Ciudad("MIA","Miami","USA"),new Ciudad("CAN","Cancun","Mexico"),new Avion("NV506","Boeing","737-400","2001",168,28,6),d,true,200,""),
-            new Vuelo(9,new Ciudad("MAD","Madrid","Espana"),new Ciudad("ROM","Roma","Italia"),new Avion("NVCR0314","Airbus","380-800","2012",450,50,9),d,true,200,""),
-            new Vuelo(10,new Ciudad("SJO","San Jose","Costa Rica"),new Ciudad("MAD","Madrid","Espana"),new Avion("TI-CR5698","Airbus","330-300","2005",297,33,9),d,true,100,""),
-            new Vuelo(11,new Ciudad("MIA","Miami","USA"),new Ciudad("SJO","San Jose","Costa Rica"),new Avion("TI-BGV","Boeing","737-600","2001",132,22,6),d,true,40,""),
-            new Vuelo(12,new Ciudad("VIE","Viena","Austria"),new Ciudad("MAD","Madrid","Espana"),new Avion("NV123","Boeing","747-400","2010",315,35,9),d,true,60,""),
-            new Vuelo(13,new Ciudad("SJO","San Jose","Costa Rica"),new Ciudad("MIA","Miami","USA"),new Avion("TI-CR","Boeing","737-800","2005",180,30,6),d,true, 100,""),
-            new Vuelo(14,new Ciudad("MIA","Miami","USA"),new Ciudad("CAN","Cancun","Mexico"),new Avion("NV506","Boeing","737-400","2001",168,28,6),d,true,200,""),
-            new Vuelo(15,new Ciudad("MAD","Madrid","Espana"),new Ciudad("ROM","Roma","Italia"),new Avion("NVCR0314","Airbus","380-800","2012",450,50,9),d,true,200,""),
-            new Vuelo(16,new Ciudad("SJO","San Jose","Costa Rica"),new Ciudad("MAD","Madrid","Espana"),new Avion("TI-CR5698","Airbus","330-300","2005",297,33,9),d,true,100,""),
-            new Vuelo(17,new Ciudad("MIA","Miami","USA"),new Ciudad("SJO","San Jose","Costa Rica"),new Avion("TI-BGV","Boeing","737-600","2001",132,22,6),d,true,40,""),
-            new Vuelo(18,new Ciudad("VIE","Viena","Austria"),new Ciudad("MAD","Madrid","Espana"),new Avion("NV123","Boeing","747-400","2010",315,35,9),d,true,60,"")
+            new Vuelo(1,new Ciudad("SJO","San Jose","Costa Rica"),new Ciudad("MIA","Miami","USA"),true, 100,""),
+            new Vuelo(2,new Ciudad("MIA","Miami","USA"),new Ciudad("CAN","Cancun","Mexico"),true,200,""),
+            new Vuelo(3,new Ciudad("MAD","Madrid","Espana"),new Ciudad("ROM","Roma","Italia"),true,200,""),
+            new Vuelo(4,new Ciudad("SJO","San Jose","Costa Rica"),new Ciudad("MAD","Madrid","Espana"),true,100,""),
+            new Vuelo(5,new Ciudad("MIA","Miami","USA"),new Ciudad("SJO","San Jose","Costa Rica"),true,40,""),
+            new Vuelo(6,new Ciudad("VIE","Viena","Austria"),new Ciudad("MAD","Madrid","Espana"),true,60,""),
+            new Vuelo(7,new Ciudad("SJO","San Jose","Costa Rica"),new Ciudad("MIA","Miami","USA"),true, 100,""),
+            new Vuelo(8,new Ciudad("MIA","Miami","USA"),new Ciudad("CAN","Cancun","Mexico"),true,200,""),
+            new Vuelo(9,new Ciudad("MAD","Madrid","Espana"),new Ciudad("ROM","Roma","Italia"),true,200,""),
+            new Vuelo(10,new Ciudad("SJO","San Jose","Costa Rica"),new Ciudad("MAD","Madrid","Espana"),true,100,""),
+            new Vuelo(11,new Ciudad("MIA","Miami","USA"),new Ciudad("SJO","San Jose","Costa Rica"),true,40,""),
+            new Vuelo(12,new Ciudad("VIE","Viena","Austria"),new Ciudad("MAD","Madrid","Espana"),true,60,""),
+            new Vuelo(13,new Ciudad("SJO","San Jose","Costa Rica"),new Ciudad("MIA","Miami","USA"),true, 100,""),
+            new Vuelo(14,new Ciudad("MIA","Miami","USA"),new Ciudad("CAN","Cancun","Mexico"),true,200,""),
+            new Vuelo(15,new Ciudad("MAD","Madrid","Espana"),new Ciudad("ROM","Roma","Italia"),true,200,""),
+            new Vuelo(16,new Ciudad("SJO","San Jose","Costa Rica"),new Ciudad("MAD","Madrid","Espana"),true,100,""),
+            new Vuelo(17,new Ciudad("MIA","Miami","USA"),new Ciudad("SJO","San Jose","Costa Rica"),true,40,""),
+            new Vuelo(18,new Ciudad("VIE","Viena","Austria"),new Ciudad("MAD","Madrid","Espana"),true,60,"")
         };
         return new ArrayList(Arrays.asList(vuelos));
     }
