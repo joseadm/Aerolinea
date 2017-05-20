adbc80a285ecc1d06114f27c62dacbcdd8796811
Jose Manuel <josemhp709@gmail.com>
2017-05-20 12:16:24 -0600

    Reservacion actualizado

--- Aerolinea/src/java/airline/model/Reservacion.java
+++ Aerolinea/src/java/airline/model/Reservacion.java
@@ -15,18 +15,25 @@ public class Reservacion implements Serializable, Jsonable{
     Viaje viaje1;
     Viaje viaje2;
     Tiquete tiquete;
-    Asiento asiento;
+    Asiento asiento1;
+    Asiento asiento2;
+    int numeroAsiento1;
+    int numeroASiento2;
     Date fecha_reserva;
 
-    public Reservacion(String codigo, Viaje viaje1, Viaje viaje2, Tiquete tiquete, Asiento asiento, Date fecha_reserva) {
+    public Reservacion(String codigo, Viaje viaje1, Viaje viaje2, Tiquete tiquete, Asiento asiento1, Asiento asiento2, int numeroAsiento1, int numeroASiento2, Date fecha_reserva) {
         this.codigo = codigo;
         this.viaje1 = viaje1;
         this.viaje2 = viaje2;
         this.tiquete = tiquete;
-        this.asiento = asiento;
+        this.asiento1 = asiento1;
+        this.asiento2 = asiento2;
+        this.numeroAsiento1 = numeroAsiento1;
+        this.numeroASiento2 = numeroASiento2;
         this.fecha_reserva = fecha_reserva;
     }
-    
+
+   
 
     public Reservacion() {
     }
@@ -35,49 +42,74 @@ public class Reservacion implements Serializable, Jsonable{
         return codigo;
     }
 
-    public Viaje getViaje1() {
-        return viaje1;
-    }
-
-    public Viaje getViaje2() {
-        return viaje2;
-    }
-
-    public Date getFecha_reserva() {
-        return fecha_reserva;
-    }
-
-    public Tiquete getTiquete() {
-        return tiquete;
-    }
-
-    public Asiento getAsiento() {
-        return asiento;
-    }
-
     public void setCodigo(String codigo) {
         this.codigo = codigo;
+    }
+
+    public Viaje getViaje1() {
+        return viaje1;
     }
 
     public void setViaje1(Viaje viaje1) {
         this.viaje1 = viaje1;
     }
 
+    public Viaje getViaje2() {
+        return viaje2;
+    }
+
     public void setViaje2(Viaje viaje2) {
         this.viaje2 = viaje2;
     }
 
-    public void setFecha_reserva(Date fecha_reserva) {
-        this.fecha_reserva = fecha_reserva;
+    public Tiquete getTiquete() {
+        return tiquete;
     }
 
     public void setTiquete(Tiquete tiquete) {
         this.tiquete = tiquete;
     }
 
-    public void setAsiento(Asiento asiento) {
-        this.asiento = asiento;
+    public Asiento getAsiento1() {
+        return asiento1;
     }
+
+    public void setAsiento1(Asiento asiento1) {
+        this.asiento1 = asiento1;
+    }
+
+    public Asiento getAsiento2() {
+        return asiento2;
+    }
+
+    public void setAsiento2(Asiento asiento2) {
+        this.asiento2 = asiento2;
+    }
+
+    public int getNumeroAsiento1() {
+        return numeroAsiento1;
+    }
+
+    public void setNumeroAsiento1(int numeroAsiento1) {
+        this.numeroAsiento1 = numeroAsiento1;
+    }
+
+    public int getNumeroASiento2() {
+        return numeroASiento2;
+    }
+
+    public void setNumeroASiento2(int numeroASiento2) {
+        this.numeroASiento2 = numeroASiento2;
+    }
+
+    public Date getFecha_reserva() {
+        return fecha_reserva;
+    }
+
+    public void setFecha_reserva(Date fecha_reserva) {
+        this.fecha_reserva = fecha_reserva;
+    }
+
 
     
 }
