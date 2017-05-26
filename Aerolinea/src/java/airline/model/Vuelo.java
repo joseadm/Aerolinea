55e72695b33ff018b05e306fc249fd00f2f502b7
Jose Manuel <josemhp709@gmail.com>
2017-05-26 14:16:48 -0600

    vuelo.java

--- Aerolinea/src/java/airline/model/Vuelo.java
+++ Aerolinea/src/java/airline/model/Vuelo.java
@@ -23,8 +23,9 @@ public class Vuelo implements Serializable, Jsonable{
     boolean oferta; // si el vuelo esta en oferta lo tira en el carrusel
     String imagen; //Direccion de la imagen
     String dia;
+    int descuento;
 
-    public Vuelo(int numero_vuelo, Ciudad ciudad_origen, Ciudad ciudad_destino, boolean estado, int precio, int duracion, int hora, boolean oferta, String imagen, String dia) {
+    public Vuelo(int numero_vuelo, Ciudad ciudad_origen, Ciudad ciudad_destino, boolean estado, int precio, int duracion, int hora, boolean oferta, String imagen, String dia, int descuento) {
         this.numero_vuelo = numero_vuelo;
         this.ciudad_origen = ciudad_origen;
         this.ciudad_destino = ciudad_destino;
@@ -35,8 +36,9 @@ public class Vuelo implements Serializable, Jsonable{
         this.oferta = oferta;
         this.imagen = imagen;
         this.dia = dia;
+        this.descuento = descuento;
     }
-    
+
     public Vuelo() {
     }
 
@@ -44,80 +46,89 @@ public class Vuelo implements Serializable, Jsonable{
         return numero_vuelo;
     }
 
-    public Ciudad getCiudad_origen() {
-        return ciudad_origen;
-    }
-
-    public Ciudad getCiudad_destino() {
-        return ciudad_destino;
-    }
-
-    public boolean isEstado() {
-        return estado;
-    }
-
-    public int getPrecio() {
-        return precio;
-    }
-
-    public int getDuracion() {
-        return duracion;
-    }
-
-    public int getHora() {
-        return hora;
-    }
-
-    public boolean isOferta() {
-        return oferta;
-    }
-
-    public String getImagen() {
-        return imagen;
-    }
-
-    public String getDia() {
-        return dia;
-    }
-
     public void setNumero_vuelo(int numero_vuelo) {
         this.numero_vuelo = numero_vuelo;
+    }
+
+    public Ciudad getCiudad_origen() {
+        return ciudad_origen;
     }
 
     public void setCiudad_origen(Ciudad ciudad_origen) {
         this.ciudad_origen = ciudad_origen;
     }
 
+    public Ciudad getCiudad_destino() {
+        return ciudad_destino;
+    }
+
     public void setCiudad_destino(Ciudad ciudad_destino) {
         this.ciudad_destino = ciudad_destino;
+    }
+
+    public boolean isEstado() {
+        return estado;
     }
 
     public void setEstado(boolean estado) {
         this.estado = estado;
     }
 
+    public int getPrecio() {
+        return precio;
+    }
+
     public void setPrecio(int precio) {
         this.precio = precio;
+    }
+
+    public int getDuracion() {
+        return duracion;
     }
 
     public void setDuracion(int duracion) {
         this.duracion = duracion;
     }
 
+    public int getHora() {
+        return hora;
+    }
+
     public void setHora(int hora) {
         this.hora = hora;
+    }
+
+    public boolean isOferta() {
+        return oferta;
     }
 
     public void setOferta(boolean oferta) {
         this.oferta = oferta;
     }
 
+    public String getImagen() {
+        return imagen;
+    }
+
     public void setImagen(String imagen) {
         this.imagen = imagen;
+    }
+
+    public String getDia() {
+        return dia;
     }
 
     public void setDia(String dia) {
         this.dia = dia;
     }
-  
+
+    public int getDescuento() {
+        return descuento;
+    }
+
+    public void setDescuento(int descuento) {
+        this.descuento = descuento;
+    }
+
+    
 }
