3332ad6a9427b4cf359ce497bd751eb6bc2cca97
Jose Manuel <josemhp709@gmail.com>
2017-05-26 13:28:26 -0600

    Viaje con hora de llegada

--- Aerolinea/src/java/airline/model/Viaje.java
+++ Aerolinea/src/java/airline/model/Viaje.java
@@ -15,12 +15,14 @@ public class Viaje implements Serializable, Jsonable{
     Date fecha;
     Avion avion;
     Vuelo vuelo;
+    int horaLlegada;
 
-    public Viaje(int numero_viaje,Date fecha, Avion avion, Vuelo vuelo) {
+    public Viaje(int numero_viaje, Date fecha, Avion avion, Vuelo vuelo, int horaLlegada) {
         this.numero_viaje = numero_viaje;
         this.fecha = fecha;
         this.avion = avion;
         this.vuelo = vuelo;
+        this.horaLlegada = horaLlegada;
     }
 
     public Viaje() {
@@ -29,31 +31,42 @@ public class Viaje implements Serializable, Jsonable{
     public int getNumero_viaje() {
         return numero_viaje;
     }
-     public Date getFecha() {
+
+    public void setNumero_viaje(int numero_viaje) {
+        this.numero_viaje = numero_viaje;
+    }
+
+    public Date getFecha() {
         return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
     }
 
     public Avion getAvion() {
         return avion;
     }
 
-    public Vuelo getVuelo() {
-        return vuelo;
-    }
-
-    public void setNumero_viaje(int numero_viaje) {
-        this.numero_viaje = numero_viaje;
-    }
-    public void setFecha(Date fecha) {
-        this.fecha = fecha;
-    }
-
     public void setAvion(Avion avion) {
         this.avion = avion;
+    }
+
+    public Vuelo getVuelo() {
+        return vuelo;
     }
 
     public void setVuelo(Vuelo vuelo) {
         this.vuelo = vuelo;
     }
-    
+
+    public int getHoraLlegada() {
+        return horaLlegada;
+    }
+
+    public void setHoraLlegada(int horaLlegada) {
+        this.horaLlegada = horaLlegada;
+    }
+
+   
 }
