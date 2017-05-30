4910f85887653f3b8bdd0e00eed6eaf43b4b4b79
AndreyCh <andreychaverri5757@gmail.com>
2017-05-30 02:10:22 -0600

    filtro de seguridad

--- Aerolinea/src/java/airline/security/SecurityFilter.java
+++ Aerolinea/src/java/airline/security/SecurityFilter.java
@@ -4,11 +4,83 @@
  * and open the template in the editor.
  */
 package airline.security;
-
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import javax.servlet.DispatcherType;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import airline.model.Usuario;
 /**
  *
  * @author AndreyCh
  */
-public class SecurityFilter {
+@WebFilter(filterName = "ReservationFilter", urlPatterns = {"/*"}, dispatcherTypes = {DispatcherType.REQUEST}, initParams = {
+    @WebInitParam(name = "TipoUsuario", value = "1")})
+public class SecurityFilter implements Filter{
+
+    List<String> clientActions = Arrays.asList("/reserva.jsp","/usuario.jsp");
+    List<String> managerActions = Arrays.asList("/tablero.jsp","/aviones.jsp","/ciudades.jsp","/viajes.jsp","/vuelos.jsp","/AirlineService_viajesListAll"
+    ,"/AirlineService_avionesListAll","/AirlineService_vuelosListAll","/AirlineService_usuariosListAll","/AirlineService_avionAdd"
+    ,"/AirlineService_ciudadAdd","/AirlineService_viajeAdd","/AirlineService_vueloAdd","/AirlineService_ciudadDelete","/AirlineService_viajeDelete",
+    "/AirlineService_vueloDelete","/AirlineService_avionDelete");
+    List<String>[] userActions;
+    
+     public SecurityFilter() {
+        userActions = (List<String>[]) new List[3];
+        userActions[1]=clientActions; 
+        userActions[2]=managerActions;
+    }    
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+         HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse resp = (HttpServletResponse) response;
+        HttpSession ses = req.getSession();
+        String LOGIN= "/Aerolinea/login.jsp";
+        String SEGURIDAD = "/Aerolinea/errorSeguridad.jsp";
+        String uri= req.getRequestURI().substring(req.getContextPath().length());
+        if (uri.equals("/Aerolinea")){
+            String action = request.getParameter("action");
+            if (action==null){
+                resp.sendRedirect(SEGURIDAD);
+            }
+            uri = uri+"_"+action;
+        }
+        if (!(userActions[1].contains(uri) || userActions[2].contains(uri) )){ // open access
+            chain.doFilter(request,response);
+        }
+        else {  // restricted access
+             Usuario user = (Usuario)ses.getAttribute("usuario");
+             if(user==null){
+                 resp.sendRedirect(LOGIN);
+             }
+             else{
+                 if (userActions[user.getTipo()].contains(uri)){
+                     chain.doFilter(request,response);
+                 }
+                 else{
+                     resp.sendRedirect(SEGURIDAD);
+                 }
+            }
+        }
+    }
+
+    @Override
+    public void destroy() {
+        
+    }
+     @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
     
 }
---
+++ Aerolinea/web/errorSeguridad.jsp
@@ -0,0 +1,59 @@
+<%-- 
+    Document   : errorSeguridad
+    Created on : May 30, 2017, 1:46:50 PM
+    Author     : AndreyCh
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Error</title>
+        <%@ include file="Imports.jspf" %>
+        <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <%@ include file="Header.jspf" %>
+
+        <div style="text-align:center;">
+            <h1 > ¡Error de seguridad!</h1>
+            <image src="images/candado.png" style="display: block; margin: auto; width: 200px">
+
+        </div>
+
+        <script> // Model
+            function Model() {
+                this.Model();
+            }
+
+            Model.prototype = {
+                Model: function () {
+                }
+            };
+        </script>
+        <script> // Controller
+            function Controller(model, view) {
+                this.Controller(model, view);
+            }
+
+            Controller.prototype = {
+                Controller: function (model, view) {
+                    this.model = model;
+                    this.view = view;
+                }
+            };
+        </script>
+        <script> // View
+            var model;
+            var controller;
+            function pageLoad(event) {
+                model = new Model();
+                controller = new Controller(model, window);
+            }
+            document.addEventListener("DOMContentLoaded", pageLoad);
+        </script>       
+    </body>
+</html>
