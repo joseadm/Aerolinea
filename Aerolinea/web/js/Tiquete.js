ab8af7be8e9339e735c4b815bdf13ad03f9ed11a
AndreyCh <andreychaverri5757@gmail.com>
2017-05-31 16:11:06 -0600

    cambio en tiquete.js

--- Aerolinea/web/js/Tiquete.js
+++ Aerolinea/web/js/Tiquete.js
@@ -1,5 +1,5 @@
-function Tiquete(codigo,usuario,nombre_pasajero,apellidos_pasajero,pasaporte_pasajero){
-	this.Tiquete(codigo,usuario,nombre_pasajero,apellidos_pasajero,pasaporte_pasajero);
+function Tiquete(codigo,usuario,nombre_pasajero,apellidos_pasajero,pasaporte_pasajero,codigo_reservacion,codigo_asiento){
+	this.Tiquete(codigo,usuario,nombre_pasajero,apellidos_pasajero,pasaporte_pasajero,codigo_reservacion,codigo_asiento);
 }
 Tiquete.prototype={
         codigo:0,
@@ -7,12 +7,17 @@ Tiquete.prototype={
 	nombre_pasajero:"",
 	apellidos_pasajero:"",
 	pasaporte_pasajero:"",
-	Tiquete:function(codigo,usuario,nombre_pasajero,apellidos_pasajero,pasaporte_pasajero){
+        codigo_reservacion: 0,
+        codigo_asiento: 0,
+	Tiquete:function(codigo,usuario,nombre_pasajero,apellidos_pasajero,pasaporte_pasajero,codigo_reservacion,codigo_asiento){
 		this.codigo=codigo;
 		this.usuario=usuario;
 		this.nombre_pasajero=nombre_pasajero;
 		this.apellidos_pasajero=apellidos_pasajero;
 		this.pasaporte_pasajero=pasaporte_pasajero;
+                this.codigo_reservacion = codigo_reservacion;
+                this.codigo_asiento = codigo_asiento;
+                
 	},
         toString:function(){
 	  return this.usuario;
@@ -20,7 +25,7 @@ Tiquete.prototype={
 }
 Tiquete.from = function(plain){
     tiquete = new Tiquete(plain.codigo,plain.usuario,plain.nombre_pasajero,plain.apellidos_pasajero,
-                          plain.pasaporte_pasajero);
+                          plain.pasaporte_pasajero,plain.codigo_reservacion,plain.codigo_asiento);
     return tiquete;
 };
 
@@ -31,6 +36,8 @@ Tiquete.to = function(tiquete){
         usuario: tiquete.usuario,
 	nombre_pasajero: tiquete.nombre_pasajero,
 	apellidos_pasajero: tiquete.apellidos_pasajero,
-	pasaporte_pasajero: tiquete.pasaporte_pasajero
+	pasaporte_pasajero: tiquete.pasaporte_pasajero,
+        codigo_reservacion : tiquete.codigo_reservacion,
+        codigo_asiento : tiquete.codigo_asiento
     };
 };
