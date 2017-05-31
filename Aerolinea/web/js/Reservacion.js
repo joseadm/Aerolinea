271d2a67b62b6e21f0fb69bb20224d2ddae3616a
AndreyCh <andreychaverri5757@gmail.com>
2017-05-31 16:13:23 -0600

    cambio en reservacion.js

--- Aerolinea/web/js/Reservacion.js
+++ Aerolinea/web/js/Reservacion.js
@@ -3,22 +3,18 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-function Reservacion(codigo,viaje1,viaje2,tiquete,asiento,fecha_reserva){
-	this.Reservacion(codigo,viaje1,viaje2,tiquete,asiento,fecha_reserva);
+function Reservacion(codigo,viaje1,viaje2,fecha_reserva){
+	this.Reservacion(codigo,viaje1,viaje2,fecha_reserva);
 }
 Reservacion.prototype={
         codigo: 0,
         viaje1: 0,
         viaje2: 0,
-        tiquete: 0,
-        asiento: 0,
         fecha_reserva:"",
-	Reservacion:function(codigo,viaje1,viaje2,tiquete,asiento,fecha_reserva){
+	Reservacion:function(codigo,viaje1,viaje2,fecha_reserva){
 		this.codigo=codigo;
 		this.viaje1=viaje1;
                 this.viaje2=viaje2;
-		this.tiquete=tiquete;
-                this.asiento = asiento;
                 this.fecha_reserva= fecha_reserva;
 	},
         toString:function(){
@@ -26,8 +22,7 @@ Reservacion.prototype={
 	}
 }
 Reservacion.from = function(plain){
-    reservacion = new Reservacion(plain.codigo,plain.viaje1,plain.viaje2,plain.tiquete,plain.asiento,
-                                  plain.fecha_reserva);
+    reservacion = new Reservacion(plain.codigo,plain.viaje1,plain.viaje2,plain.fecha_reserva);
     return reservacion;
 };
 Reservacion.to = function(reserva){
@@ -36,8 +31,6 @@ Reservacion.to = function(reserva){
         codigo: reserva.codigo,
         viaje1: reserva.viaje1,
         viaje2: reserva.viaje2,
-        tiquete: reserva.tiquete,
-        asiento: reserva.asiento,
         fecha_reserva: reserva.fecha_reserva
     };
 };
